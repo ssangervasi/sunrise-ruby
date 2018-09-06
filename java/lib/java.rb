837c28af75083ca6f0cb7941c65e06ef516930e4
Sebastian Sangervasi <ssangervasi@squareup.com>
2018-09-05 17:35:08 -0700

    Implement Java.void

--- java/lib/java.rb
+++ java/lib/java.rb
@@ -1,6 +1,16 @@
 module Java
   def void(method_name)
-    puts "Method :#{method_name} declared void"
+    method_name.tap do
+      original_method = instance_method(method_name)
+      define_method(method_name) do |*args, &block|
+        result = original_method.bind(self).call(*args, &block)
+        return if result.nil?
+        fail TypeError, <<~ERROR_MESSAGE.gsub("\n", ' ')
+          Attemp to return non-nil value of type #{result.class}
+          from void method \##{:method_name}
+        ERROR_MESSAGE
+      end
+    end
   end
 end
 
--- java/spec/java_spec.rb
+++ java/spec/java_spec.rb
@@ -1,5 +1,4 @@
 require 'rspec'
-
 require 'java'
 
 RSpec.describe Java do
@@ -8,21 +7,84 @@ RSpec.describe Java do
   it('is trash') { is_expected.to eq ActualLiteralTrash }
   it('is sane') { is_expected.to eq TheOnlySaneLanguage }
 
-
   describe '.void' do
-    subject(:class_with_void_method) do
+    subject(:klass) do
       class ExampleClass
         extend Java
 
-        void def basic_void_method
+        attr_reader :side_effect
+
+        void def honest_void_method(side_effect_value = nil)
+          @side_effect = side_effect_value
           nil
+        end
+
+        void def deceitful_void_method(side_effect_value = nil)
+          @side_effect = side_effect_value
+          'non-nil value'
+        end
+
+        private void def private_deceitful_void_method(side_effect_value = nil)
+          @side_effect = side_effect_value
+          'non-nil value'
         end
       end
       ExampleClass
     end
 
-    it 'works' do
-      expect(class_with_void_method.new).to_not be nil
+    it 'defines the class' do
+      expect(klass.new).to_not be nil
+    end
+
+    describe '#honest_void_method' do
+      it 'returns nil' do
+        expect(klass.new.honest_void_method).to be nil
+      end
+
+      it 'executes the original method body' do
+        instance = klass.new
+        instance.honest_void_method('side effect')
+        expect(instance.side_effect).to eq 'side effect'
+      end
+    end
+
+    describe '#deceitful_void_method' do
+      it 'raises a TypeError' do
+        expect { klass.new.deceitful_void_method }
+          .to raise_error(TypeError)
+      end
+
+      it 'executes the original body' do
+        instance = klass.new
+        begin
+          instance.deceitful_void_method('side effect')
+        rescue TypeError; end
+        expect(instance.side_effect).to eq 'side effect'
+      end
+    end
+
+    describe '#private_deceitful_void_method' do
+      context 'when called normally' do
+        it 'raises a NoMethodError' do
+          expect { klass.new.private_deceitful_void_method }
+            .to raise_error(NoMethodError)
+        end
+      end
+
+      context 'when called with #send' do
+        it 'raises a TypeError' do
+          expect { klass.new.send(:private_deceitful_void_method) }
+            .to raise_error(TypeError)
+        end
+
+        it 'executes the original body' do
+          instance = klass.new
+          begin
+            instance.send(:private_deceitful_void_method, 'side effect')
+          rescue TypeError; end
+          expect(instance.side_effect).to eq 'side effect'
+        end
+      end
     end
   end
 end
