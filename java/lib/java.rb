e2022f1d212580d7530a93fad773b64871e232a2
Sebastian Sangervasi <ssangervasi@squareup.com>
2018-09-06 09:21:47 -0700

    Void should not raise error, kept void_strict

--- java/lib/java.rb
+++ java/lib/java.rb
@@ -3,6 +3,16 @@ module Java
     method_name.tap do
       original_method = instance_method(method_name)
       define_method(method_name) do |*args, &block|
+        original_method.bind(self).call(*args, &block)
+        nil
+      end
+    end
+  end
+
+  def void_strict(method_name)
+    method_name.tap do
+      original_method = instance_method(method_name)
+      define_method(method_name) do |*args, &block|
         result = original_method.bind(self).call(*args, &block)
         return if result.nil?
         fail TypeError, <<~ERROR_MESSAGE.gsub("\n", ' ')
--- java/spec/java_spec.rb
+++ java/spec/java_spec.rb
@@ -14,17 +14,73 @@ RSpec.describe Java do
 
         attr_reader :side_effect
 
-        void def honest_void_method(side_effect_value = nil)
+        void def non_nil_method(side_effect_value = 'non nil value')
+          @side_effect = side_effect_value
+        end
+
+        private void def private_non_nil_method(side_effect_value = 'non nil value')
+          @side_effect = side_effect_value
+        end
+      end
+      ExampleClass
+    end
+
+    it 'defines the class' do
+      expect(klass.new).to_not be nil
+    end
+
+    describe '#non_nil_method' do
+      it 'returns nil' do
+        expect(klass.new.non_nil_method).to be nil
+      end
+
+      it 'executes the original method body' do
+        instance = klass.new
+        instance.non_nil_method('side effect')
+        expect(instance.side_effect).to eq 'side effect'
+      end
+    end
+
+    describe '#private_non_nil_method' do
+      context 'when called normally' do
+        it 'raises a NoMethodError' do
+          expect { klass.new.private_non_nil_method }
+            .to raise_error(NoMethodError)
+        end
+      end
+
+      context 'when called with #send' do
+        it 'returns nil' do
+          expect(klass.new.send(:private_non_nil_method)).to eq nil
+        end
+
+        it 'executes the original body' do
+          instance = klass.new
+          instance.send(:private_non_nil_method, 'side effect')
+          expect(instance.side_effect).to eq 'side effect'
+        end
+      end
+    end
+  end
+
+  describe '.void_strict' do
+    subject(:klass) do
+      class ExampleClass
+        extend Java
+
+        attr_reader :side_effect
+
+        void_strict def honest_void_method(side_effect_value = nil)
           @side_effect = side_effect_value
           nil
         end
 
-        void def deceitful_void_method(side_effect_value = nil)
+        void_strict def deceitful_void_method(side_effect_value = nil)
           @side_effect = side_effect_value
           'non-nil value'
         end
 
-        private void def private_deceitful_void_method(side_effect_value = nil)
+        private void_strict def private_deceitful_void_method(side_effect_value = nil)
           @side_effect = side_effect_value
           'non-nil value'
         end
